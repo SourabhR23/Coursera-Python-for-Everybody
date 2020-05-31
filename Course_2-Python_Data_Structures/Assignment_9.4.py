10012b681d23679c5cd46ee20aefd26c27deb7c5
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2020-05-31 15:20:45 +0530

    Assignment Solution

--- Course_2-Python_Data_Structures/Assignment_9.4.py
+++ Course_2-Python_Data_Structures/Assignment_9.4.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. 
 The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program 
 creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. 
