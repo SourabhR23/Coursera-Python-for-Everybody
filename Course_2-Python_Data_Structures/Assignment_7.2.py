62246e6aed825b2889deabe08732e15c165c7ed3
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2020-05-31 15:17:37 +0530

    Assignment Solution

--- Course_2-Python_Data_Structures/Assignment_7.2.py
+++ Course_2-Python_Data_Structures/Assignment_7.2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
 X-DSPAM-Confidence:    0.8475
 Count these lines and extract the floating point values from each of the lines and compute the average of those values and 
@@ -12,6 +6,7 @@ You can download the sample data at http://www.py4e.com/code3/mbox-short.txt whe
 mbox-short.txt as the file name."""
 
 # Use the file name mbox-short.txt as the file name
+
 fname = input("Enter file name: ")
 fh = open(fname)
 count = 0
