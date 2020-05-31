e3ce8be39d0b4c6cc009ca672067ed6756af9233
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2020-05-31 15:21:32 +0530

    Assignment Solution

--- Course_2-Python_Data_Structures/Assignment_10.2.py
+++ Course_2-Python_Data_Structures/Assignment_10.2.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of 
 the messages. You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second 
 time using a colon.
