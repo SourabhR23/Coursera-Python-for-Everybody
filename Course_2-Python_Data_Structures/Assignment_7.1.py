8eddcaa2b602b725f3f6b62a5f3fd8779b0ecdbb
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2020-05-31 15:16:22 +0530

    Assignment Solution

--- Course_2-Python_Data_Structures/Assignment_7.1.py
+++ Course_2-Python_Data_Structures/Assignment_7.1.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 """7.1 Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents
 of the file in upper case. Use the file words.txt to produce the output below.
 You can download the sample data at http://www.py4e.com/code3/words.txt"""
@@ -18,4 +12,3 @@ for line in fh:
 inp = fh.read()
 f = inp.upper()
 print (f[:])
-
